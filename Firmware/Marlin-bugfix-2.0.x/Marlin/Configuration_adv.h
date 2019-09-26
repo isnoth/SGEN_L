5d4467da2ea2d46d5221615ef36b50af3d48a287
isnoth <isnothe@gmail.com>
2019-09-26 09:39:12 +0800

    SD改成从LCD读取

--- Firmware/Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
+++ Firmware/Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
@@ -963,7 +963,7 @@
 
   // LPC-based boards have on-board SD Card options. Override here or defaults apply.
   #ifdef TARGET_LPC1768
-    //#define LPC_SD_LCD          // Use the SD drive in the external LCD controller.
+    #define LPC_SD_LCD          // Use the SD drive in the external LCD controller.
     //#define LPC_SD_ONBOARD      // Use the SD drive on the control board. (No SD_DETECT_PIN. M21 to init.)
     //#define LPC_SD_CUSTOM_CABLE // Use a custom cable to access the SD (as defined in a pins file).
     //#define USB_SD_DISABLED     // Disable SD Card access over USB (for security).
